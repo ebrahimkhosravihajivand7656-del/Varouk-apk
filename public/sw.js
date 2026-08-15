de2eaafa9ab2f9b39e1060877c8571557fd55a8b
ebrahimkhosravihajivand7656-del <ebrahimkhosravihajivand7656@gmail.com>
2026-08-16 03:17:56 +0330

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,1 @@
+const CACHE='varouk-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/index.html']))));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
